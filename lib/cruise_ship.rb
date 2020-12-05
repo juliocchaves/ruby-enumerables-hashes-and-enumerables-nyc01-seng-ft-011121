f0869037da930cf3029674705a22964ee6e0161a
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-05 19:39:42 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,7 +6,7 @@ passengers = {
  suite_e: "Crumpet the Elf"
  }
 
-def select_winner(passengers)
+#def select_winner(passengers)
   # add the code snippet here!
   winner = ""
 passengers.each do |suite, name|
